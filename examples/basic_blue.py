18a07d87e20b1ce9b73a72803ddac79176884c5e
Johannes Lange <johannes.lange@rwth-aachen.de>
2018-04-23 10:30:14 +0200

    CR header in examples

--- examples/basic_blue.py
+++ examples/basic_blue.py
@@ -2,6 +2,21 @@
 
 # test of the example in Nucl. Instrum. Meth. A 270 (1988) 110
 
+# Copyright 2018 Johannes Lange
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import blue_combine as blue
 
 measurements = blue.Measurements([9.5, 11.9, 11.1, 8.9])
--- examples/reduced_correlations.py
+++ examples/reduced_correlations.py
@@ -2,6 +2,21 @@
 
 # same example as `uncertainty_sources.py`, but using reduced correlations
 
+# Copyright 2018 Johannes Lange
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import blue_combine as blue
 import numpy as np
 from collections import OrderedDict
